b612631c6052264a2b794c997cb4f57575cb21e3
yury-poloshkov <1286600@gmail.com>
2024-09-09 23:38:56 +0300

    homework22 reviews add-function completed

--- homework22/javascript/review.js
+++ homework22/javascript/review.js
@@ -1,9 +1,28 @@
+function readReviews(key) {
+    // let reviews = new Map();
+    return localStorage.getItem("reviews")
+        ? new Map(JSON.parse(localStorage.getItem("reviews")))
+        : new Map();
+    // return reviews;
+}
+
 function saveReview(productName, productReview) {
-    const reviews = new Map();
-    reviews.set(JSON.parse(localStorage.getItem("reviews")));
-    reviews.set(productName, productReview);
-    console.log(reviews);
-    localStorage.setItem("reviews", JSON.stringify(reviews));
+    let reviews = readReviews("reviews");
+    // let reviews = new Map();
+    // if (localStorage.getItem("reviews")) {
+    //     reviews = new Map(JSON.parse(localStorage.getItem("reviews")));
+    if (reviews.get(productName) != null) {
+        reviews.get(productName).push(productReview);
+    } else {
+        reviews.set(productName, [productReview]);
+    }
+    // } else {
+    //     reviews.set(productName, [productReview]);
+    // }
+    localStorage.setItem(
+        "reviews",
+        JSON.stringify(Array.from(reviews.entries()))
+    );
 }
 
 export { saveReview };
--- homework22/javascript/review_add.js
+++ homework22/javascript/review_add.js
@@ -4,12 +4,15 @@ const btnAddReview = document.querySelector(".review_save_btn");
 
 btnAddReview.addEventListener("click", (event) => {
     event.preventDefault();
-    const productName = document.querySelector(".review_product_name").value;
-    const productReview = document.querySelector(".review_text").value;
+    const productName = document
+        .querySelector(".review_product_name")
+        .value.trim();
+    const productReview = document.querySelector(".review_text").value.trim();
     if (!productName || !productReview) {
         alert("Заполните все поля формы!");
         throw new Error("ERROR: data missing");
     } else {
         saveReview(productName, productReview);
+        window.location.href = "./index.html";
     }
 });
